6ec7dd47506d308b89d79e5c7708dcf3d0ad6312
jantunez <angelo.antunez@interseguro.com.pe>
2024-07-30 21:16:11 -0500

    postgress connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,26 @@
 import { Module } from '@nestjs/common';
 import { ArticulosModule } from './articulos/articulos.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import environments  from '../utils/environments';
+
+if (!environments.DATABASE_URL || !environments.DATABASE_USER || !environments.DATABASE_PASSWORD) {
+  throw new Error('DATABASE_URL environment variable is not set');
+}
+
+const dbUrlObj = new URL(environments.DATABASE_URL);
+
+const host = dbUrlObj. hostname;
+const database = dbUrlObj.pathname.substring(1);
 
 @Module({
   imports: [
     ArticulosModule,
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3307,
-      username: 'articulos_crud',
-      password: 'root',
-      database: 'db_crud',
+      type: 'postgres',//mysql
+      host: host,
+      username: environments.DATABASE_USER,
+      password: environments.DATABASE_PASSWORD,
+      database: database,
       //entities: [],
       autoLoadEntities: true,
       synchronize: true,
---
+++ utils/environments.ts
@@ -0,0 +1,5 @@
+export default{
+    DATABASE_URL: process.env.LOG_LEVEL || 'postgres://mahmud.db.elephantsql.com/iblrcnaa',
+    DATABASE_USER: process.env.DATABASE_USER || 'iblrcnaa',
+    DATABASE_PASSWORD: process.env.DATABASE_PASSWORD || 'NzSFyMEaa2hAOZ1NJxizLJjXNR8r8pz6'
+}
